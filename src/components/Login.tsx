097180116bb3bb0dfea7b6915f5dcd7788d0f1f0
jessica-sobreira <j.jecristina@gmail.com>
2024-01-23 19:39:39 -0300

    estilo login

---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const LoginStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,9 +1,28 @@
+import { LoginStyled } from "../components/Login";
 
 
 export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+        <LoginStyled>
+            <div id="esquerdo">
+                <h1>Growtweet</h1>
+                <h3>Trabalho final do bloco intermediário</h3>
+                <p>O Growtweet é a plataforma definitiva para todos os apaixonados<br></br>
+                    por redes sociais que buscam uma experiência familiar e poderosa,
+                    semelhante ao Twitter, mas com um toque único. <br></br>Seja parte desta comunidade
+                    que valoriza a liberdade de expressão, <br></br>a conexão com pessoas de todo o mundo
+                    e a disseminação de ideias.
+                </p>
+            </div>
+
+            <div id="direito">
+                <h2>Entrar no Growtweet</h2>
+                <form>
+                    <input type="text" id="email" placeholder="Email" />
+                    <input type="password" id="password" placeholder="Senha" />
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
+        </LoginStyled>
+    );
 }

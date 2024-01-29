ae1717b090b135adf9361057e9b7672e4ed57c78
jessica-sobreira <j.jecristina@gmail.com>
2024-01-29 18:22:43 -0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { router } from './pages/router';
+import { routes } from './pages/routes.tsx';
 import { RouterProvider } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-      <RouterProvider router={router} />
+      <RouterProvider router={routes} />
   )
